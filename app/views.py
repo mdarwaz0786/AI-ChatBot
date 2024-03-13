2c111627f0ed19169ab64cde51202fe9ae0e53d4
Md Arwaz <mdarwaznew2017@gmail.com>
2024-03-14 02:53:41 +0530

    fix bug

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from .models import Chat
 from django.utils import timezone
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 openai_api_key = settings.OPENAI_API_KEY
 openai.api_key = openai_api_key
@@ -22,14 +23,15 @@ def ask_openai(message):
     return answer
 
 # Create your views here.
+@login_required
 def chatbot(request):
-    chats = Chat.objects.filter(user=request.user.id)
+    chats = Chat.objects.filter(user=request.user)
 
     if request.method == 'POST':
         message = request.POST.get('message')
         response = ask_openai(message)
 
-        chat = Chat(user=request.user.id, message=message, response=response, created_at=timezone.now())
+        chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
         chat.save()
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html', {'chats': chats})
