6c82da5ec5ffd27a8af21a7d3ddec54e62ec970f
Md Arwaz <mdarwaznew2017@gmail.com>
2024-03-14 00:01:56 +0530

    deploy

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'RGdBAwrisFrmOFDCALsrEFDwlaOuXGYR',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '27234',
     }
 }
 
